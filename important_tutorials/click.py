fdae6bf87685ff56278092a494a01552a48284c1
mb8144 <108338883+mb8144@users.noreply.github.com>
2022-10-05 14:38:39 +0200

    Update click.py

--- important_tutorials/click.py
+++ important_tutorials/click.py
@@ -1,6 +1,6 @@
-# Dieses Skript wird benötigt, damit Colab während dem Trainieren des
-# Models nicht disconnected. Es führt einen simplen Linksclick alle 3min durch.
-# Der Crusor wird eine Zeile unterhalb platziert.
+"""Dieses Skript wird benötigt, damit Colab während dem Trainieren des
+Models nicht disconnected. Es führt einen simplen Linksclick alle 3min durch.
+Der Crusor wird eine Zeile unterhalb platziert."""
 
 from pynput.mouse import Controller, Button
 import time
