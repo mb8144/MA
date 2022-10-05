6cedadab595cd6bb3ae642fd4256698224096a76
mb8144 <108338883+mb8144@users.noreply.github.com>
2022-10-05 14:10:55 +0200

    Add files via upload

---
+++ important_tutorials/click.py
@@ -0,0 +1,13 @@
+# Dieses Skript wird benötigt, damit Colab während dem Trainieren des
+# Models nicht disconnected. Es führt einen simplen Linksclick alle 3min durch.
+# Der Crusor wird eine Zeile unterhalb platziert.
+
+from pynput.mouse import Controller, Button
+import time
+mouse = Controller()
+
+while True:
+    mouse.click(Button.left, 1)
+    print("clicked")
+
+    time.sleep(180)
