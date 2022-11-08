c25ceeaa615e48a9e56c33584b2784456a9fcb15
mb8144 <108338883+mb8144@users.noreply.github.com>
2022-11-08 17:15:58 +0100

    Update resize.py

--- data_preprocessing/resize.py
+++ data_preprocessing/resize.py
@@ -1,3 +1,8 @@
+"""
+Skript wurde erstellt, um alle Bilder
+des finalen Datensets(ca. 19'000) mit der Grösse (224, 224)zu
+komprimierern.
+"""
 import cv2
 import glob
 import numpy as np
@@ -8,9 +13,10 @@ out_path = '<path>/'
 image_paths = list(glob.glob(input_path))
 
 for i, img in enumerate(image_paths):
+    img_size = 224
     try:
         image = cv2.imread(img)
-        image = cv2.resize(image, (224, 224))
+        image = cv2.resize(image, (img_size, img_size))
         image_bw = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     except:
         continue
