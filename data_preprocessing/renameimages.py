2139256a1739da83c1a554d2a80763b694675c99
mb8144 <108338883+mb8144@users.noreply.github.com>
2022-11-27 12:18:32 +0100

    Update renameimages.py

--- data_preprocessing/renameimages.py
+++ data_preprocessing/renameimages.py
@@ -1,10 +1,20 @@
+"""
+Skript für die strukturierte Umbenennung 
+aller Bilder
+"""
 import os
 
+# Ordner, in welchem die Bilder liegen
+# werden festgelegt
 inputdirpath = ""
 path = os.chdir(inputdirpath)
+
+# Erstellen der beiden "Klassen"
 class1 = "miss"
 class2 = "make"
 
+# Bilder werden je nach Klasse untetschiedlich
+# umbenannt und nummeriert
 i = 0
 for file in os.listdir(path):
     new_file_name = class1 + "{}.jpg".format(i)
